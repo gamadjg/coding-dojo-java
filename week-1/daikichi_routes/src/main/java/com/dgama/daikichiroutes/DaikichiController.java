b9642ecebb7c4664142bdd1dca7ae3ba31ca238f
gamadjg <gamadavid36@gmail.com>
2022-12-02 13:34:47 -0800

    Assignment, daikichi routes, complete

---
+++ week-1/daikichi_routes/src/main/java/com/dgama/daikichiroutes/DaikichiController.java
@@ -0,0 +1,32 @@
+package com.dgama.daikichiroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DaikichiController {
+	public static void main(String[] args) {
+		SpringApplication.run(com.dgama.daikichiroutes.DaikichiController.class, args);
+	}
+
+	@RequestMapping("/")
+	public String landing(){
+		return "Hello World!";
+	}
+	@RequestMapping("/daikichi")
+	public String welcome(){
+		return "Welcome!";
+	}
+	@RequestMapping("/daikichi/today")
+	public String today(){
+		return "Today you will find luck in all your endeavors!";
+	}
+	@RequestMapping("/daikichi/tomorrow")
+	public String tomorrow(){
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+
+}
