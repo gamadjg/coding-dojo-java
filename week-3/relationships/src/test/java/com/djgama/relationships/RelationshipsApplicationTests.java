bee6a72566558b96cf37c2e39fce3691c608c9f9
gamadjg <gamadavid36@gmail.com>
2022-12-14 04:00:19 -0800

    new test assignment for 1:1 & 1:m relationships setup

---
+++ week-3/relationships/src/test/java/com/djgama/relationships/RelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djgama.relationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
